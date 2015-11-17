ce6750508964b1474e6d803fa94c580d4e3b2a0b
Harlow Ward <harlow@hward.com>
2015-11-16 20:39:52 -0800

    Update return values from Header Get

--- authtoken.go
+++ authtoken.go
@@ -3,34 +3,40 @@
 package authtoken
 
 import (
-  "fmt"
-  "net"
-  "net/http"
+	"encoding/base64"
+	"errors"
+	"net/http"
+	"strings"
+)
+
+const (
+	BASIC_SCHEMA  string = "Basic "
+	BEARER_SCHEMA string = "Bearer "
 )
 
 // FromRequest extracts the auth token from req.
 func FromRequest(req *http.Request) (string, error) {
-  // Grab the raw Authoirzation header
-  authHeader, err := req.Header.Get("Authorization")
-  if err != nil {
-    return nil, errors.New("Authorization header required")
-  }
+	// Grab the raw Authoirzation header
+	authHeader := req.Header.Get("Authorization")
+	if authHeader == "" {
+		return "", errors.New("Authorization header required")
+	}
 
-  // Confirm the request is sending Basic Authentication credentials.
-  if !strings.HasPrefix(authHeader, BASIC_SCHEMA) && !strings.HasPrefix(authHeader, BEARER_SCHEMA) {
-    return nil, errors.New("Authorization requires Basic/Bearer scheme")
-  }
+	// Confirm the request is sending Basic Authentication credentials.
+	if !strings.HasPrefix(authHeader, BASIC_SCHEMA) && !strings.HasPrefix(authHeader, BEARER_SCHEMA) {
+		return "", errors.New("Authorization requires Basic/Bearer scheme")
+	}
 
-  // Get the token from the request header
-  // The first six characters are skipped - e.g. "Basic ".
-  if strings.HasPrefix(authHeader, BASIC_SCHEMA) {
-    str, err := base64.StdEncoding.DecodeString(authHeader[len(BASIC_SCHEMA):])
-    if err != nil {
-      return nil, errors.New("Base64 encoding issue")
-    }
-    creds := strings.Split(string(str), ":")
-    return creds[0], nil
-  } else {
-    return authHeader[len(BEARER_SCHEMA):], nil
-  }
+	// Get the token from the request header
+	// The first six characters are skipped - e.g. "Basic ".
+	if strings.HasPrefix(authHeader, BASIC_SCHEMA) {
+		str, err := base64.StdEncoding.DecodeString(authHeader[len(BASIC_SCHEMA):])
+		if err != nil {
+			return "", errors.New("Base64 encoding issue")
+		}
+		creds := strings.Split(string(str), ":")
+		return creds[0], nil
+	} else {
+		return authHeader[len(BEARER_SCHEMA):], nil
+	}
 }
